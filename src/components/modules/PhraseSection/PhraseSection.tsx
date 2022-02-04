5ae006f183e6c77d08ed3bf1ffb4780d8a194b5f
Tomas <tomas_forman@outlook.com>
2022-02-04 00:34:58 -0300

    add avatars

--- src/components/modules/PhraseSection/PhraseSection.tsx
+++ src/components/modules/PhraseSection/PhraseSection.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './PhraseSection.module.scss';
+import { Box } from '@chakra-ui/react';
+import Image from 'next/image';
 
 type Props = {
   mainText: string;
@@ -14,6 +16,10 @@ const PhraseSection = ({mainText, secondaryText, textColor = '#6e7b62', bgColor 
     <div className={styles.phraseContainer} style={{color: textColor, backgroundColor: bgColor}}>
       <p className={styles.main}>{mainText}</p>
       <p className={styles.secondary}>{secondaryText}</p>
+      <Box d={'flex'} justifyContent={'center'} alignItems={'center'}>
+        <Image src={'/avatar-ella.png'} width={100} height={150}/>
+        <Image src={'/avatar-el.png'} width={100} height={150}/>
+      </Box>
     </div>
   )
 }
